8a46069adc5e21826175e45b40dfd2c1170b6201
s1034512 <s0903750375@gmail.com>
2025-06-09 00:35:36 +0800

    Add files via upload

---
+++ append2.js
@@ -0,0 +1,20 @@
+$(()=>{
+    $(".banner").append(`
+        <div class="nav">
+            <h2>千と千尋</h2>
+            <div class="as">
+                <a href="index.html">首頁</a>
+                <a href="01.html">劇情簡介</a>
+                <a href="02.html">人物介紹</a>
+                <a href="03.html">作者簡介</a>
+                <a href="04.html">其他</a>
+            </div>
+        </div>
+    `)
+    $(".footer").append(`
+        <h2>千と千尋の神隠し</h2>
+        <div class="copyright">
+            &copy;千と千尋の神殺し
+        </div>
+    `)
+})
